81c9a1411e634610cfc881c69e1c1f6946155cf6
Inger Klekacz <ingernet@gmail.com>
2018-06-13 16:52:11 -0700

    disabling webhook because firewall blues

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -1,15 +1,15 @@
 class profile::r10k {
     class {'r10k':
         remote => 'https://github.com/ingernet/pet_control_repo.git',
-}
-
-    class {'r10k::webhook::config':
-        use_mcollective => false,
-        enable_ssl => false,
     }
 
-    class {'r10k::webhook':
-        user => 'root',
-        group => 'root',
-    }
+    # class {'r10k::webhook::config':
+        # use_mcollective => false,
+        # enable_ssl => false,
+    # }
+
+    # class {'r10k::webhook':
+        # user => 'root',
+        # group => 'root',
+    # }
 }
